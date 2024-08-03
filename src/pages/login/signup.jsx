f52b24d0e9fda1f7539bb9d2dc725608c9a35592
ImComy <ahmedcomy74@gmail.com>
2024-08-03 20:12:48 +0300

    getting /signup ready for the backend

--- src/pages/login/signup.jsx
+++ src/pages/login/signup.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import './login.css'; // Same CSS file as Login component
+import './login.css';
 import Navigation from '../../assets/navigation/navigation';
 import Footer from '../../assets/footer/footer';
+import axios from 'axios';
 
 function Signup() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    firstname: '',
+    lastname: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/signup', formData); // No need to specify the full URL
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data); // Handle errors here
+    }
+  };
+
   return (
     <div className='container'>
       <Navigation IsSigned={'false'} className='navigation' />
       <div className='content'>
         <p className='Login'>Signup</p>
         <p className='Login-welcome'>Create your account</p>
-        <input type='text' placeholder='Email' className='inputelement' />
-        <div className='input-flex'>
-          <input type='text' placeholder='First Name' className='inputelement' />
-          <input type='text' placeholder='Last Name' className='inputelement' />
-        </div>
-        <input type='password' placeholder='Password' className='inputelement' />
-        <button className='submit'>Signup</button>
+        <form onSubmit={handleSubmit} className='form'>
+          <input type='text' placeholder='Email' className='inputelementmail' value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+          <div className='input-flex'>
+            <input type='text' placeholder='First Name' className='inputelement' value={formData.firstname} onChange={(e) => setFormData({ ...formData, firstname: e.target.value })} />
+            <input type='text' placeholder='Last Name' className='inputelement' value={formData.lastname} onChange={(e) => setFormData({ ...formData, lastname: e.target.value })} />
+          </div>
+          <input type='password' placeholder='Password' className='inputelementpass' value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+          <button className='submit' type='submit'>Signup</button>
+        </form>
         <div className='OR'>
           <div className='line'></div>
           <p>OR</p>
